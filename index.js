45ffea273ae8897edb6868afe9c63224ae8adbf0
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-02 02:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 
-function scuberGreetingForFeet(ride){
-  if (ride <= 400) {
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
     return 'This one is on me!'
-    } else if (ride > 2000) {
+  } else if (feet > 2000) {
         return 'I will gladly take your thirty bucks.'
       } else  {
           return 'No can do.'
         }
       }
     }
+}
+
+/*function scuberGreetingForFeet (feet) {
+/*
+  if (feet <= 400) {
+
+    return 'This one is on me!';
+
+  } else if (1999 < feet && feet < 2500) {
+
+    return "I will gladly take your thirty bucks.";
+
+  } else {
+
+    return "No can do.";
+
+  }
+
+}
+
+/*
+
+function ternaryCheckCity (city) {
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+}
+
+
+
+function switchOnCharmFromTip (tip) {
+
+  switch (tip) {
+
+    case 'generous':
+
+      return 'Thank you so much.';
+
+    case 'not as generous':
+
+      return 'Thank you.';
+
+    default:
+
+      return 'Bye.';
+
+  }
+
 }
