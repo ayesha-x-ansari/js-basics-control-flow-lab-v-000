57751378cf40d48e840a60baf90740a56eda9c18
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-02 02:40:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 
-function scuberGreetingForFeet(feet){
-  if (feet <= 400) {
-    return 'This one is on me!';
-  } else if (feet > 2000) {
-        return 'I will gladly take your thirty bucks.';
-      } else  {
-          return 'No can do.';
-        }
-      }
-    }
-}
 
-/*function scuberGreetingForFeet (feet) {
-/*
+function scuberGreetingForFeet (feet) {
+
   if (feet <= 400) {
 
     return 'This one is on me!';
