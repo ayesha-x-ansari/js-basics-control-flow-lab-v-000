e94313e227eda6c3dcd5b9a5cfcc1986bb6de141
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-02 02:19:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(ride){
     return 'This one is on me!'
       if (ride > 2000) {
         return 'I will gladly take your thirty bucks.'
-        else (ride) > 2500){
+        else (ride) >= 2500){
           return 'No can do.'
         }
       }
